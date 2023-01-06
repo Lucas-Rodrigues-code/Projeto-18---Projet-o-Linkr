561cf2b9ea4f7f795632d8eea9827eb811acc5a5
Pedro <malpvpandtextures@gmail.com>
2023-01-05 22:18:58 -0300

    mkpost e getpost finalizados

--- src/database/db.js
+++ src/database/db.js
@@ -4,13 +4,19 @@ dotenv.config();
 
 const { Pool } = pg;
 
-const configDatabase = {
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-};
+//const configDatabase = {
+  //connectionString: process.env.DATABASE_URL,
+ // ssl: {
+  //  rejectUnauthorized: false,
+ // },
+//};
 
+<<<<<<< HEAD
 const connection = new Pool(configDatabase);
+=======
+ const configDatabase = {
+  connectionString: process.env.DATABASE_URL
+ };
+>>>>>>> aefd3a8 (mkpost e getpost finalizados)
 
 export default connection;
