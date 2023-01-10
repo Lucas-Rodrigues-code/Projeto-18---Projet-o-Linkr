dd1d4b3b8a1464e9278b50f3d2befa37d663a7db
Pedro <malpvpandtextures@gmail.com>
2023-01-10 16:47:21 -0300

    add route posts by id

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 
-import { likePost, unlikePost, mkPost, getPosts, getPostsByUserId } from "../controllers/posts.controller.js";
-import { mkPostValidation, tokenValidation } from "../middlewares/posts.validation.middleware.js";
+import  { likePost, unlikePost,mkPost,getPosts,getPostsByUserId } from "../controllers/posts.controller.js";
+import { mkPostValidation } from "../middlewares/posts.validation.middleware.js";
 import userRouter from "./user.router.js"
 //importe suas rotas aqui
 const router = Router();
@@ -10,7 +10,8 @@ router.use(userRouter);
 
 router.post("/timeline", mkPostValidation,mkPost)
 router.get("/timeline", getPosts)
-router.post("/likes/:postId", tokenValidation, likePost);
-router.delete("/likes/:postId", tokenValidation, unlikePost);
+router.get("/timeline/:id",getPostsByUserId)
+router.post("/likes/:postId", likePost);
+router.delete("/likes/:postId", unlikePost);
 
 export default router;
